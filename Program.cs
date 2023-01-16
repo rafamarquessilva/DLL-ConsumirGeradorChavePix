c5373885f3c3411605fd1eac00e9afcd865a93bf
Rafael Marques <rifacrp100@hotmail.com>
2023-01-16 15:56:13 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GeradorChavePix;
+
+Console.WriteLine("-----------------");
+Console.WriteLine("GERADOR CHAVE PIX");
+Console.WriteLine("-----------------");
+
+Console.Write("Quantidade a ser gerada de chaves: ");
+
+var quantidadeChaves = int.Parse(Console.ReadLine());
+
+var chavesPix = GeradorPix.GetChavesPix(quantidadeChaves);
+
+chavesPix.ForEach(chave => Console.WriteLine(chave));
